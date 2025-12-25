779ede30003c74112465fbfbb26f4fd23ba0e30a
Nick <angertina777@ukr.net>
2025-12-25 19:59:54 +0200

    fixed struct 1.1

--- Home/js/pagination.js
+++ Home/js/pagination.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const mangaList = [
-        { title: "Solo Leveling", img1x: "/Home/images/manga/solo-leveling-1x.jpg", img2x: "/Home/images/manga/solo-leveling-2x.jpg", link: "#" },
-        { title: "Second Life Ranker", img1x: "/Home/images/manga/second-life-ranker-1x.jpg", img2x: "/Home/images/manga/second-life-ranker-2x.jpg", link: "#" },
-        { title: "The only one who returned with the maximum level", img1x: "/Home/images/manga/the-only-one-who-returned-with-the-maximum-level-1x.jpg", img2x: "/Home/images/manga/the-only-one-who-returned-with-the-maximum-level-2x.jpg", link: "#" },
-        { title: "Naruto", img1x: "/Home/images/manga/naruto-1x.jpg", img2x: "/Home/images/manga/naruto-2x.jpg", link: "#" },
-        { title: "The Descent of the Demonic Master", img1x: "/Home/images/manga/the-descent-of-the-demonic-master-1x.jpg", img2x: "/Home/images/manga/the-descent-of-the-demonic-master-2x.jpg", link: "#" },
-        { title: "Sonna kazoku nara sutechaeba?", img1x: "/Home/images/manga/sonna-kazoku-nara-sutechaeba-1x.jpg", img2x: "/Home/images/manga/sonna-kazoku-nara-sutechaeba-2x.jpg", link: "#" },
-        { title: "Agnyeoneun Marionette", img1x: "/Home/images/manga/agnyeoneun-marionette-1x.jpg", img2x: "/Home/images/manga/agnyeoneun-marionette-2x.jpg", link: "#" },
-        { title: "The Beginning After the End", img1x: "/Home/images/manga/the-beginning-after-the-end-1x.jpg", img2x: "/Home/images/manga/the-beginning-after-the-end-2x.jpg", link: "#" },
-        { title: "I fell in love after school", img1x: "/Home/images/manga/i-fell-in-love-after-school-1x.jpg", img2x: "/Home/images/manga/i-fell-in-love-after-school-2x.jpg", link: "#" }
+        { title: "Solo Leveling", img1x: "./Home/images/manga/solo-leveling-1x.jpg", img2x: "./Home/images/manga/solo-leveling-2x.jpg", link: "#" },
+        { title: "Second Life Ranker", img1x: "./Home/images/manga/second-life-ranker-1x.jpg", img2x: "./Home/images/manga/second-life-ranker-2x.jpg", link: "#" },
+        { title: "The only one who returned with the maximum level", img1x: "./Home/images/manga/the-only-one-who-returned-with-the-maximum-level-1x.jpg", img2x: "./Home/images/manga/the-only-one-who-returned-with-the-maximum-level-2x.jpg", link: "#" },
+        { title: "Naruto", img1x: "./Home/images/manga/naruto-1x.jpg", img2x: "./Home/images/manga/naruto-2x.jpg", link: "#" },
+        { title: "The Descent of the Demonic Master", img1x: "./Home/images/manga/the-descent-of-the-demonic-master-1x.jpg", img2x: "./Home/images/manga/the-descent-of-the-demonic-master-2x.jpg", link: "#" },
+        { title: "Sonna kazoku nara sutechaeba?", img1x: "./Home/images/manga/sonna-kazoku-nara-sutechaeba-1x.jpg", img2x: "./Home/images/manga/sonna-kazoku-nara-sutechaeba-2x.jpg", link: "#" },
+        { title: "Agnyeoneun Marionette", img1x: "./Home/images/manga/agnyeoneun-marionette-1x.jpg", img2x: "./Home/images/manga/agnyeoneun-marionette-2x.jpg", link: "#" },
+        { title: "The Beginning After the End", img1x: "./Home/images/manga/the-beginning-after-the-end-1x.jpg", img2x: "./Home/images/manga/the-beginning-after-the-end-2x.jpg", link: "#"},
+        { title: "I fell in love after school", img1x: "./Home/images/manga/i-fell-in-love-after-school-1x.jpg", img2x: "./Home/images/manga/i-fell-in-love-after-school-2x.jpg", link: "#" }
     ];
 
     let itemsPerPage = 8;

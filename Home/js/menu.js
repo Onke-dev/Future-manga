20304933ffefbd6c360c4fa51331530fbc1820c1
Onke-dev <nadyushka.nats@gmail.com>
2025-12-27 11:29:30 +0200

    fixed menu scroll

--- Home/js/menu.js
+++ Home/js/menu.js
@@ -8,11 +8,37 @@
     modal: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.openModalBtn) refs.openModalBtn.addEventListener('click', toggleModal);
+  if (refs.closeModalBtn) refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function preventTouch(e) {
+    e.preventDefault();
+  }
+
+  function lockBodyScroll() {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    document.documentElement.dataset.scrollY = scrollY;
+    document.body.style.top = `-${scrollY}px`;
+    document.body.classList.add('is-open');
+    document.addEventListener('touchmove', preventTouch, { passive: false });
+  }
+
+  function unlockBodyScroll() {
+    document.body.classList.remove('is-open');
+    document.removeEventListener('touchmove', preventTouch);
+    const stored = document.documentElement.dataset.scrollY;
+    const scrollY = stored ? Number(stored) : 0;
+    document.body.style.top = '';
+    delete document.documentElement.dataset.scrollY;
+    window.scrollTo(0, scrollY);
+  }
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
+    if (refs.modal.classList.contains('is-open')) {
+      lockBodyScroll();
+    } else {
+      unlockBodyScroll();
+    }
   }
 })();

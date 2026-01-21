484efc4188dbc0e1d53207571ee41dac7a399c6e
Nick <angertina777@ukr.net>
2026-01-21 13:41:57 +0200

    done scroll for mobile

---
+++ assets/js/scroll-up.js
@@ -0,0 +1,16 @@
+const scrollBtn = document.querySelector(".scroll-up");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});

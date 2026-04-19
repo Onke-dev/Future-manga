f5b81544cd0eade60dc7ba1dee728d86472284fe
Onke-dev <angertina777@ukr.net>
2026-04-19 11:13:51 +0000

    Deploying to gh-pages from @ Onke-dev/Future-manga@e4a7fd40bbb1488f00dd2224c0bc0b1241843bae 🚀

---
+++ assets/header-auth-bR1olKkD.js
@@ -0,0 +1,2 @@
+import{a as o}from"./main-Cn7ueFrf.js";import{o as r}from"./vendor-Nxcx92q3.js";const c={links:document.querySelectorAll('.account-user, a[data-key="account"]')};r(o,e=>{c.links.forEach(t=>{const a="/Future-manga/";e?t.href=`${a}pages/account/user_account.html`:t.href=`${a}pages/registration/registration.html`})});
+//# sourceMappingURL=header-auth-bR1olKkD.js.map
